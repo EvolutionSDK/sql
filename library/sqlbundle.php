af237bab3ca679653340b0e9831df8e5b8fb3bc5
Kelly Becker <kellylsbkr@gmail.com>
2011-10-25 02:09:11 -0400

    updating for nates

--- library/sqlbundle.php
+++ library/sqlbundle.php
@@ -20,7 +20,7 @@ class SQLBundle {
 		 * If a relation is on the same table prefix it with its bundle name
 		 */
 		foreach($sql as $table=>$relations) {
-			if(!is_array($relations)) throw new \Exception("Invalid YAML Config Error-ing in $bundle.$table");
+			if(!is_array($relations)) throw new \Exception("Invalid YAML Config Error-ing in $dir/configure/_sql_structure.yaml");
 			foreach($relations as $kind=>$values) {
 				if($kind == 'fields' || $kind == 'singular' || $kind == 'plural') continue;
 				
