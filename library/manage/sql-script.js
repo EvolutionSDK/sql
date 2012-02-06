0c7e3f6e3eccc941fbc26cd7571f2a7672c33902
KellyLSB <kellylsbkr@gmail.com>
2012-02-06 15:39:00 -0700

    trying to get stuff organizaed

---
+++ library/manage/sql-script.js
@@ -0,0 +1,22 @@
+sql = {
+	sync: function() {
+		$('.state-init').fadeOut(500, function(e) {
+			$('.state-running').fadeIn(500, function(e) {
+				$.post('/@manage/sql/sync', new Object(), function(r) {
+					$('.state-running').fadeOut(500, function(e) {
+						$('.state-complete').fadeIn(500);
+						sql.uptd();
+					});
+				});
+			});
+		})
+	},
+	uptd: function() {
+		$('ul.categories').fadeOut(500, function(e) {
+			$.post('/@manage/sql/sync', new Object(), function(r) {
+				$('ul.categories').html(r);
+				setTimeout(function() {$('ul.categories').fadeIn(500);}, 500);
+			});
+		});
+	}
+};
